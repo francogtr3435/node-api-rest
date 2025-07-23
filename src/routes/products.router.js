c1b55718e608e97987b30e655476eaf12b6e6fda
franco gruppi <frag_123@hotmail.com.ar>
2025-07-22 22:40:52 -0300

    actualiza

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -2,7 +2,9 @@ import { Router } from "express";
 import {auth} from "../middlewares/auth.middleware.js";
 import { getAllProducts, searchProducts, getProductById, createProduct, deleteProduct, updateProduct} from "../controllers/products.controllers.js";
 
+
 const router = Router();
+
 
 router.get('/products', getAllProducts );
 router.get('/products/search',searchProducts );
