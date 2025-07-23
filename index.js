3bac2ec09d302b246ce7ed6552df0385faa76075
franco gruppi <frag_123@hotmail.com>
2025-07-22 21:54:13 -0300

    actualiza

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
- import  "dotenv/config"
- import express from "express"
- import cors from "cors"
- const app = express()
+ import  "dotenv/config";
+ import express from "express";
+ import cors from "cors";
+ const app = express();
 
- app.use(cors())
- app.use(express.json())
+ app.use(cors());
+ app.use(express.json());
 
  app.get("/", (req, res) =>{
-    res.json({message: "API REST EN NODE.JS"})
-})
+    res.json({ message: "API REST EN NODE.JS" })
+});
 
 import productsRouter from "./src/routes/products.router.js"
-app.use("/api", productsRouter)
+app.use("/api" , productsRouter);
 
-import authRouter from "./src/routes/auth.router.js"
-app.use(authRouter)
+import authRouter from "./src/routes/auth.router.js";
+app.use(authRouter);
 
 
  app.use((req,res,next) =>{
-    res.status(404).json({ error: "no encontrado"})
-})
+    res.status(404).json({ error: "no encontrado" });
+});
 
-const PORT = process.env.PORT || 3001
+//const PORT = process.env.PORT || 3001;
+//app.listen(PORT , () => console.log(`http://localhost:${PORT}`));
 
-app.listen(PORT, ()=>console.log(`http://localhost:${PORT}`))
-
+export default app;
