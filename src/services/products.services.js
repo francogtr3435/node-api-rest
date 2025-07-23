0ef93117ad9d8a6304661d4ea0d75fb99f40e774
franco gruppi <frag_123@hotmail.com.ar>
2025-07-23 14:52:39 -0300

    actualiza

--- src/services/products.services.js
+++ src/services/products.services.js
@@ -1,4 +1,4 @@
-import * as Model from "../models/products.model.js"
+import * as Model from "../models/products.model.js";
 
 export const getAllProducts = async () => {
     return await Model.getAllProducts();
